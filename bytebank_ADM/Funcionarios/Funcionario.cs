bb658fee6883e1337e9c8833a28a370a20cbf561
André Bessa <andrebessax@gmail.com>
2022-10-04 18:41:35 -0300

    Adicionar arquivos de projeto.

---
+++ bytebank_ADM/Funcionarios/Funcionario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_ADM.Funcionarios
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public double Salario { get; set; }
+
+        public double GetBonificacao()
+        {
+            return this.Salario * 0.10;
+        }
+    }
+}
---
+++ bytebank_ADM/Program.cs
@@ -0,0 +1,27 @@
+﻿using bytebank_ADM.Funcionarios;
+using bytebank_ADM.Utilitario;
+
+Funcionario pedro = new Funcionario();
+pedro.Nome = "Pedro malazartes";
+pedro.Cpf = "123456789";
+pedro.Salario = 2000;
+
+Console.WriteLine(pedro.Nome);
+Console.WriteLine(pedro.GetBonificacao());
+
+Diretor roberta = new Diretor();
+roberta.Nome = "Roberta Silva";
+roberta.Cpf = "987654321";
+roberta.Salario = 5000;
+
+Console.WriteLine(roberta.Nome);
+Console.WriteLine(roberta.GetBonificacao());
+
+GerenciadorDeBonificacao gerenciador = new GerenciadorDeBonificacao();
+gerenciador.Registrar(pedro);
+gerenciador.Registrar(roberta);
+
+Console.WriteLine("Total de bonificações: " + gerenciador.TotalDeBonficacao);
+
+
+
